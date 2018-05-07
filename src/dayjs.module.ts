147f56483deb008cb602c0548588a0d656f7eb7b
Merza, Leonardo (lm240n) <lm240n@att.com>
2018-05-07 12:06:17 -0400

    automated commit 07/05/2018 12:06:17

--- src/dayjs.module.ts
+++ src/dayjs.module.ts
@@ -1,33 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { AddPipe } from './add.pipe';
-import { CalendarPipe } from './calendar.pipe';
 import { DateFormatPipe } from './date-format.pipe';
 import { DifferencePipe } from './difference.pipe';
-import { DurationPipe } from './duration.pipe';
 import { FromUnixPipe } from './from-unix.pipe';
 import { ParsePipe } from './parse.pipe';
 import { SubtractPipe } from './subtract.pipe';
-import { TimeAgoPipe } from './time-ago.pipe';
-import { UtcPipe } from './utc.pipe';
-import { FromUtcPipe } from './from-utc.pipe';
-import { LocalTimePipe } from './local.pipe';
-import { LocalePipe } from './locale.pipe';
 
 const ANGULAR_DAYJS_PIPES = [
   AddPipe,
-  CalendarPipe,
   DateFormatPipe,
   DifferencePipe,
-  DurationPipe,
   FromUnixPipe,
   ParsePipe,
   SubtractPipe,
-  TimeAgoPipe,
-  UtcPipe,
-  FromUtcPipe,
-  LocalTimePipe,
-  LocalePipe
 ];
 
 @NgModule({
