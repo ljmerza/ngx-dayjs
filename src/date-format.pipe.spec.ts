01dc9bac07f59f2776b2342eab7513b96c671452
Merza, Leonardo (lm240n) <lm240n@att.com>
2018-05-07 11:54:46 -0400

    automated commit 07/05/2018 11:54:45

--- src/date-format.pipe.spec.ts
+++ src/date-format.pipe.spec.ts
@@ -5,8 +5,8 @@ describe('DateFormatPipe', () => {
   describe('#transform', () => {
     it('should properly format a date', () => {
       const pipe = new DateFormatPipe();
-      const result = pipe.transform('2016-01-24 01:23:45', 'MMMM D YYYY, h:mm:ss');
-      expect(result).toBe('January 24 2016, 1:23:45');
+      const result = pipe.transform('2016-01-24 01:23:45', 'MMMM D YYYY');
+      expect(result).toBe('January 24 2016');
     });
   });
 });
